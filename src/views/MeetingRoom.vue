e7426f6b094f7b5b58208089e392c091c33d2e6a
Rohmatullah Sony Wijaya <rsonywijaya@gmail.com>
2024-08-15 00:30:24 +0700

    first commit

---
+++ src/views/MeetingRoom.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from '../components/Navbar.vue';
+import Sidebar from '../components/Sidebar.vue';
+</script>
+
+<template>
+    <main>
+        <Navbar />
+        <Sidebar />
+    </main>
+</template>
